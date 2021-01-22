337eed3c82facf8eb9d8ca8da0ef74657b5d2523
brooke <brookedanae88@gmail.com>
2021-01-22 08:07:38 -0500

    Updating save file

--- Lab03/Lab03/Program.cs
+++ Lab03/Lab03/Program.cs
@@ -6,7 +6,52 @@ namespace Lab03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string response;
+
+            do
+            {
+
+                bool isInvalidResponse;
+
+                do
+                {
+
+                    Console.Write("Enter a number between 1 and 100: ");
+                    var userNumber = Console.ReadLine();
+                    var outputNumber = int.Parse(userNumber);
+
+                    if (outputNumber % 2 == 0 && outputNumber <= 25)
+                    {
+                        Console.WriteLine($"Output: {userNumber}, Even and less than 25!");
+                    }
+                    else if (outputNumber % 2 == 0 && outputNumber <= 60)
+                    {
+                        Console.WriteLine($"Output: {userNumber}, Even and less than 60!");
+                    }
+                    else if (outputNumber % 2 == 0)
+                    {
+                        Console.WriteLine($"Output: {userNumber}, Even!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Output: {userNumber}, Odd" + "\n");
+                    }
+
+                    Console.WriteLine("Continue? (y/n): ");
+                    response = Console.ReadLine();
+                    isInvalidResponse = response != "y" && response != "n";
+                    if (isInvalidResponse)
+                    {
+                        Console.WriteLine("Oops! You need to enter 'y' or 'n'!" + "\n");
+                    }
+                } while (isInvalidResponse);
+
+
+            } while (response == "y");
+
+            Console.WriteLine("Thanks for playing! Goodbye :)");
+
+
         }
     }
 }
